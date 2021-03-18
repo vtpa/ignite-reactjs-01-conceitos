426cff4d788030f2666edad68397d34ef6faab37
Vinicius Passos <viniciuspa87@hotmail.com>
2021-03-18 18:19:27 -0300

    ✔ Challenge completed!

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,24 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === "")
+      return;
+    const id = Math.ceil(Math.random() * 10000);
+    const newTask = {
+      id,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask])
+    setNewTaskTitle("")
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete } : task))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
